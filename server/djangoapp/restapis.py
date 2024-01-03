bfd9c7770f1dbb9d8bb32a6fb069477fcf051156
Pallavi7597 <pallavirai7597@gmail.com>
2024-01-02 23:38:56 -0500

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,7 +1,6 @@
 # Uncomment the imports below before you add the function code
 import requests
-import json
-from requests.auth import HTTPBasicAuth
+
 
 import os
 from dotenv import load_dotenv
@@ -14,13 +13,14 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-def get_request(endpoint, **kwargs):
-    params = ""
+
+def get_request(endpoint,**kwargs):
+    params =""
     if(kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url+endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
@@ -32,17 +32,17 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+        print(f"Unexpected {err=},{type(err)=}")
         print("Network exception occurred")
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url,json=data_dict)
         print(response.json())
